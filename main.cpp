4f07e6d2226b594d756f60b5ab9ea51624a668e1
KomarnitskyKonstantin <komarnitsky.kostya13@mail.ru>
2025-04-09 00:18:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,19 @@
 #include <iomanip> 
 int main()
 {
-	int a = 4;
-	int b = 2;
-	int c = 5;
+	float a;
+	float b;
+	float c;
+	
+	std::cin>>a;
+	std::cin>>b;
+	std::cin>>c;
+
 	float p = (1/2.0f)*(a+b+c);
 	float s = sqrt(p*(p-a)*(p-b)*(p-c));
 
-
-	std::cout<<c<<std::endl;
 	std::cout<<p<<std::endl; 
 	std::cout<<s<<std::endl;
-
-
 
 	return 0;
 }
